a3aafcd36229303f5ac6138e40d25f5e266bf08c
Unknown <ade.f@students.amikom.ac.id>
2018-10-30 06:48:40 +0700

    pertama

---
+++ MyMovie/app/src/main/java/com/reza/project/mymovie/model/MovieApiService.java
@@ -0,0 +1,10 @@
+package com.reza.project.mymovie.model;
+
+import retrofit2.Call;
+import retrofit2.http.GET;
+import retrofit2.http.Query;
+
+public interface MovieApiService {
+    @GET("movie/now_playing")
+    Call<MovieResponse> getTopRatedMovies(@Query("api_key") String apiKey);
+}
